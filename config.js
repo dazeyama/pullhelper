1665a1f4f05e05483611136fa676cb46d82cb4d2
Dazey <daisyamanita@gmail.com>
2026-06-16 05:03:55 -0700

    Configure Apps Script relay endpoint for Send PDF

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.PULLHELPER_CONFIG = {
   // URL of your deployed Google Apps Script web app (the email relay).
   // Leave blank until you deploy it (see apps-script/Code.gs for instructions).
   // Example: "https://script.google.com/macros/s/AKfycb..../exec"
-  RELAY_ENDPOINT: "",
+  RELAY_ENDPOINT: "https://script.google.com/macros/s/AKfycby1TEAoso7apSRL2QZFxOiHu1dTOSgWOuglm2QgSJcKymVlRy-O4AwYEFayWYX4jCPZ5g/exec",
 
   // Milliseconds to wait between Scryfall API calls (their guidance: 50-100ms).
   SCRYFALL_DELAY_MS: 100,
